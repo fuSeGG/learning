b24be1117e0239acf257651d786b3bbc6f4cea18
jonathanbayes <sirbayes@gmail.com>
2018-12-27 13:32:03 +0100

    Færdiggjort en save to txt fil del af ConsoleThoughts + begyndt at lege med interface i ThoughtPlatform + Slettet nogle gamle filer og lavet noget mere orden

--- ConsoleThougths/ConsoleThougths/Program.cs
+++ ConsoleThougths/ConsoleThougths/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,47 +9,42 @@ using static System.Console;
 namespace ConsoleThougths {
     class Program {
         static void Main(string[] args) {
-            Dictionary<string, string> thoughts = new Dictionary<string, string>();
 
-
-            ShowThoughts();
-            
-            ReadLine();
-
-
-
-
-            // add a thought to the list
-            void AddThought() {
-                Write("\nNew Thought:");
-                string value = ReadLine();
-                Write("Name the thought: ");
-                string key = ReadLine();
-                thoughts.Add(key, value);
-                Clear();
-                ShowThoughts();
-            }
-
-
-            // show all thoughts on list
-            void ShowThoughts() {
-                int index = 0;
-                if (thoughts.Count > 0) {                    
-                    foreach (string s in thoughts.Values) {
-                        index += 1;
-                        WriteLine($"[{index}] {s}");
-                    }
-                }
-
-                WriteLine("\n1) Add thought \n2) Remove Thought");
-                var foo = ReadLine();
-                if (Char.Equals(foo, "1")){
-                    AddThought();
-                }                
-            }
+            NewNote();
 
 
 
         }
+
+        // add a note
+        private static void NewNote() {
+            try {
+                Write("Enter filename: ");
+                string filename = ReadLine();
+                Clear();
+                Write(filename + ": ");
+                using (StreamWriter sw = File.CreateText($"{filename}.txt")) {
+                    string body = ReadLine();
+                    sw.WriteLine($"{body}");
+                    sw.Close();
+                }
+            }
+            catch (Exception e) {
+                Write(e.Message);
+            }
+        }
+
+        /*
+        private static void DeleteNote() { }
+
+        private static void ListNotes() {
+            
+        }
+        */
+
+
+
+
+
     }
 }
