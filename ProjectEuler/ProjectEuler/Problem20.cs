1ce92fe90f9a57f4b437f8c7367af81ef4606475
jonathanbayes <sirbayes@gmail.com>
2019-01-10 15:09:56 +0100

    2,75 PE problemer løst.

---
+++ ProjectEuler/ProjectEuler/Problem20.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Numerics;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+
+        /* Find the sum of each digit in !100        
+
+
+        static void Main(string[] args)
+        {
+            BigInteger fac100 = 1;
+            double sum = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                fac100 *= i;
+            }
+
+            string sumString = fac100.ToString();
+            for (int i = 0; i < sumString.Length; i++)
+            {
+                double n = Char.GetNumericValue(sumString, i);
+                sum += n;
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        } */
+    }
+}
