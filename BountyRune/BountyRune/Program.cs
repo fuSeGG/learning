c6b4f242e0feca6a8888ac8b987881f82fd465e3
jonathanbayes <sirbayes@gmail.com>
2019-01-17 15:43:50 +0100

    Learning about delegates and made a Bounty Rune calculator

---
+++ BountyRune/BountyRune/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BountyRune
+{
+    class Program
+    {
+        static void Main()
+        {
+            int bountyGold = 40;
+            int increasePerMinute = 2;
+            int bountyWorth;
+            int players = 5;
+            int runes = 4;
+
+            Console.WriteLine("Total Bounty Gold up for grabs at minute count:");
+
+            for (int minute = 0; minute <= 60; minute++)
+            {
+                bountyWorth = bountyGold + increasePerMinute * minute;
+                if (minute % 5 == 0)
+                {
+                    Console.WriteLine("{0}: " + bountyWorth * runes * players, minute);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LearningDelegates/LearningDelegates/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningDelegates
+{
+    class Program
+    {
+        int bountyValue = 40;
+        int addedBounty = 10;
+        static int bountyCount = 0;
+        
+        static void Main()
+        {
+            
+            Program obj = new Program();
+            
+            
+            Console.WriteLine(obj.NextBounty(bountyCount));            
+            
+            Console.ReadLine();
+        }
+
+        public int Udregneren(int a, int b, int c) {
+            int NetWorthSwing = a * b + c;
+            return NetWorthSwing;
+        }
+
+
+        public int NextBounty(int n)
+        {
+            return n += 5;            
+        }        
+
+        public delegate void Kladden(int num, int num2, int num3);
+    }
+}
