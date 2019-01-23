a17448bba0ed07e7dd3fa476cc6cfaf3e7c21daf
Jonathan <sirbayes@gmail.com>
2019-01-24 00:33:38 +0100

    no message

---
+++ ConsoleThougths/ConsoleThougths/DesignDocument.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleThougths
+{
+    class DesignDocument
+    {
+        /*
+        Categories:
+
+            50 years
+            10 years
+            3 years
+            1 years
+            1 month
+            1 week
+            
+            No-time
+                (unsorted, or doesn't fit with time?)
+            
+            Specific time
+                + Birthday etc.
+                + Calendar system currently deals with this, but may just be integrated so as to automatically plot it into week schedule.
+                + An optional preparation component (say for a birthday, a gift may need to be bought well ahead of the date of the event itself)
+
+        Functionality:
+            
+            Automatically updates to the current month.
+                Things that wasn't completed last month enters new month (or (special) unsorted category?)
+        
+            Visual Representation: 
+                Overview system
+                    Colour legend
+                 
+        
+            Keeps archive of completed tasks
+        
+        Design choices to be made:
+
+            Should the interface be based on the named months? Or more flexibly the next 30 days? Fluid vs Rigid system.
+
+
+        Potential add-on:
+            
+            Gamification
+                Point system
+        */
+
+    }
+}
