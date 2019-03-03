cd5800d751f56eff8a2a0e71eb802183af7d73ac
Jonathan <sirbayes@gmail.com>
2019-03-03 13:34:39 +0100

    no message

--- PreFlopPlayer/PreFlopPlayer/BetState.cs
+++ PreFlopPlayer/PreFlopPlayer/BetState.cs
@@ -8,13 +8,15 @@ namespace PreFlopPlayer {
     public class BetState {
 
         // En counter der starter på 0 og stiger med 1 hver gang nogle forøger betsize i runden (PF starter på 1 pga BB)
-        public int betstate { get; set; }
+        public int nBet { get; set; }
 
         // En token man tager når man forøger betsize. (så der er kun 1 der kan have den)
-        public string initiator { get; set; }
+        public bool aggressorInitiative { get; set; }
 
         // true if someone could C-bet this or next street.
         public bool CBetOpen { get; set; }     
+
+        // Stack size and pot?
 
     }
 }
